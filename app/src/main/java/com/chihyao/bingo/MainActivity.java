001b0c071b7b689f91a3d1e2c75fd2d7ff9ea95b
RogerLi <>
2021-07-21 17:06:04 +0800

    firebase database save login displayname

--- app/src/main/java/com/chihyao/bingo/MainActivity.java
+++ app/src/main/java/com/chihyao/bingo/MainActivity.java
@@ -1,14 +1,85 @@
 package com.chihyao.bingo;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        FirebaseAuth.getInstance().removeAuthStateListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_menu_signout:
+                FirebaseAuth.getInstance().signOut();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull @NotNull FirebaseAuth auth) {
+        FirebaseUser user = auth.getCurrentUser();
+        if (user == null) {
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                                    new AuthUI.IdpConfig.GoogleBuilder().build()
+                            ))
+                            .setIsSmartLockEnabled(false)
+                            .build()
+                    ,RC_SIGN_IN);
+        } else {
+            Log.d(TAG, "onAuthStateChanged: " + auth.getCurrentUser().getEmail() + "/" + auth.getCurrentUser().getUid());
+            String displayName = user.getDisplayName();
+            FirebaseDatabase.getInstance("https://bingo-e33cc-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                    .getReference("users")
+                    .child(user.getUid())
+                    .child("displayName")
+                    .setValue(user.getDisplayName());
+        }
+
     }
 }
